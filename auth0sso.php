e20b2737b5a3f78a47351795d6bdd38677561a9e
Jon Erickson <jon@deschutesdesigngroup.com>
2020-01-19 17:06:47 -0800

    Updated config options. Added full logout flow.

--- auth0sso.php
+++ auth0sso.php
@@ -24,14 +24,32 @@ function auth0sso_config() {
                 "FriendlyName" => "Auth0 Domain",
                 "Type" => "text",
                 "Size" => "25",
+                "Description" => "<br>Your Auth0 application domain. If you are using a custom domain with Auth0, enter it here. Please make sure to include the scheme (http/s).",
                 "Placeholder" => "www.example.auth0.com" ),
             "clientid" => array(
                 "FriendlyName" => "Client ID",
                 "Type" => "text",
-                "Size" => "25" ),
+                "Size" => "25",
+                "Description" => "<br>Your Auth0 application Client ID." ),
             "clientsecret" => array(
                 "FriendlyName" => "Client Secret",
                 "Type" => "password",
-                "Size" => "25" )
+                "Size" => "25",
+                "Description" => "<br>Your Auth0 application Client Secret." ),
+            "fulllogout"  => array(
+                "FriendlyName" => "Full Logout",
+                "Type" => "yesno",
+                "Description" => "When a user logs out of WHMCS locally, also log out of Auth0." ),
+            "logoutredirect" => array(
+                "FriendlyName" => "Logout Redirect",
+                "Type" => "text",
+                "Size" => "25",
+                "Description" => "<br>Enter the URL you would like a user redirected to after they logout. The URL must be configured in Auth0 under Allowed Logout URLs.",
+                "Placeholder" => "www.example.com" ),
+            "autoauth" => array(
+                "FriendlyName" => "AutoAuth Key",
+                "Type" => "password",
+                "Size" => "25",
+                "Description" => "<a target='_blank' href='https://docs.whmcs.com/AutoAuth'>Enable AutoAuth</a><br>Follow the linked instructions to enable Aut0Auth within WHMCS then enter the AutoAuth key here.")
     ));
 }
--- hooks.php
+++ hooks.php
@@ -9,27 +9,87 @@ if ( !defined( "WHMCS" ) ) {
 include_once( __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php' );
 
 use Jumbojett\OpenIDConnectClient;
+use Jumbojett\OpenIDConnectClientException;
+use WHMCS\Module\Addon\Setting;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 /**
- * Client Area Hook
+ * Client Area Login Hook
  */
-add_hook('ClientAreaPage', 1, function( $vars ) {
+add_hook('ClientAreaPageLogin', 1, function( $vars ) {
 
     // If no user logged in
     if ( !$_SESSION['uid'] )
     {
-        // Start our authentication code flow
-        $oidc = new OpenIDConnectClient( 'https://sso.deschutesdesigngroup.com', CLIENT_ID, CLIENT_SECRET );
-        $oidc->addScope( array( 'profile email openid' ) );
-        $oidc->authenticate();
+        // Try and get our settings
+        try
+        {
+            // Get our domain
+            $domain = Setting::where( 'module', 'auth0sso' )->where( 'setting', 'domain' )->firstOrFail();
+            $clientid = Setting::where( 'module', 'auth0sso' )->where( 'setting', 'clientid' )->firstOrFail();
+            $clientsecret = Setting::where( 'module', 'auth0sso' )->where( 'setting', 'clientsecret' )->firstOrFail();
+            $autoauth = Setting::where( 'module', 'auth0sso' )->where( 'setting', 'autoauth' )->firstOrFail();
 
-        // Get our email
-        $email = $oidc->requestUserInfo('email');
+            // Start our authentication code flow
+            $oidc = new OpenIDConnectClient( $domain->value, $clientid->value, $clientsecret->value );
+            $oidc->addScope( array( 'profile email openid' ) );
+            $oidc->authenticate();
 
-        // Use AutoAuth to log the user in
-        $timestamp = time();
-        $hash = sha1( $email . $timestamp . AUTOAUTH_KEY );
-        $url = $_SERVER['HTTPS'] == 'on' ? 'https://' : 'http://' . $_SERVER['SERVER_NAME'] . "/dologin.php?email=$email&timestamp=$timestamp&hash=$hash&goto=" . urlencode( 'clientarea.php?action=products' );
-        header("Location: $url");
+            // Get our email
+            $email = $oidc->requestUserInfo('email');
+
+            // Use AutoAuth to log the user in
+            $timestamp = time();
+            $hash = sha1( $email . $timestamp . $autoauth->value );
+            $url = $vars['systemurl'] . "dologin.php?email=$email&timestamp=$timestamp&hash=$hash&goto=" . urlencode( 'clientarea.php?action=products' );
+            header("Location: $url");
+        }
+
+        // Catch our exceptions if we cant find any settings
+        catch ( ModelNotFoundException $exception )
+        {
+            // Log our errors
+            logActivity( 'Auth0SSO Model Exception: ' . $exception->getMessage() );
+        }
+
+        // Catch our exceptions if we cant create an OIDC client object
+        catch ( OpenIDConnectClientException $exception )
+        {
+            // Log our errors
+            logActivity( 'Auth0SSO Client Exception: ' . $exception->getMessage() );
+        }
+    }
+});
+
+/**
+ * Client Logout Hook
+ */
+add_hook('ClientLogout', 1, function( $vars )
+{
+    // Try and get our settings
+    try
+    {
+        // Get logout
+        $logout = Setting::where( 'module', 'auth0sso' )->where( 'setting', 'fulllogout' )->firstOrFail();
+
+        // If we are wanting a full logout
+        if ( $logout->value == 'on' )
+        {
+            // Get our domain
+            $domain = Setting::where( 'module', 'auth0sso' )->where( 'setting', 'domain' )->firstOrFail();
+            $clientid = Setting::where( 'module', 'auth0sso' )->where( 'setting', 'clientid' )->firstOrFail();
+            $redirect = Setting::where( 'module', 'auth0sso' )->where( 'setting', 'logoutredirect' )->firstOrFail();
+
+            // Logout of URL
+            $url = $domain->value . "/v2/logout?client_id=$clientid->value&returnTo=$redirect->value";
+            header( "Location: $url" );
+        }
+    }
+
+    // Catch our exceptions if we cant find any settings
+    catch ( ModelNotFoundException $exception )
+    {
+        // Log our errors
+        logActivity( 'Auth0SSO Model Exception: ' . $exception->getMessage() );
     }
 });
